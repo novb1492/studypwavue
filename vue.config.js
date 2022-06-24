27126f2737cdfe400f579d5cfa62f018f52e7a58
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-24 13:28:19 +0900

    --

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https: true,
+    //https: true,
 
   },
 })
